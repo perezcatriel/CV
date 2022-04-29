f88821e0eb886d044c0cc59945f717c6f7823549
perezcatriel <perezcatriel@gmail.com>
2022-04-29 12:58:58 -0300

    Giving life to the page with JS

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function toggle() {
-	let $menu = document.querySelector("nav");
+	let $menu = document.querySelector(".contactos");
 
 	switch ($menu.style.display) {
-	case "none":
-		mostrar($menu);
-		break;
-	default:
-		ocultar($menu);
-		break;
+		case "none":
+			mostrar($menu);
+			break;
+		default:
+			ocultar($menu);
+			break;
 	}
 }
 
@@ -19,8 +19,41 @@ function ocultar($menu) {
 	$menu.style.display = "none";
 }
 
-window.scroll({
-  top: 100,
-  left: 100,
-  behavior: 'smooth'
-});
+function sLeft() {
+	const $cardsProject = document.querySelector("div.proyectos-container");
+
+	$cardsProject.scroll(0, 0);
+}
+
+function sRight() {
+	const $cardsProject = document.querySelector("div.proyectos-container");
+
+	let now = window.scrollX;
+	console.log(now)
+	/* let next = 250;
+
+	let click = now + next;
+
+	$cardsProject.scroll(click, 0);
+
+
+	 */
+
+	$cardsProject.scroll(1000, 0);
+}
+
+function sLeftE() {
+	const $cardsProject = document.querySelector("div.education-container");
+
+	$cardsProject.scroll(0, 0);
+}
+
+function sRightE() {
+	const $cardsProject = document.querySelector("div.education-container");
+
+	$cardsProject.scroll(1000, 0);
+}
+
+function clickMe() {
+	console.log("Click!");
+}
